a3a5138057eb409925d99184cc3c71abaa893c1a
Jan Maciejewski <u162_janmac_waw@technischools.com>
2026-03-24 11:20:00 +0100

    uruchomienie

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+import os
+
 from app import create_app
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host=host, port=port, debug=False)
